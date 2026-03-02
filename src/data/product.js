b25ad87e87a7655390e4b00f9640a5266c9cc3e1
chels-path <chelsbytheway7@gmail.com>
2026-03-02 09:01:13 +0100

    Create product.js

---
+++ src/data/product.js
@@ -0,0 +1,10 @@
+// src/data/product.js
+const product = {
+  name: "MacBook Pro 16\"",
+  price: "$2,499.99",
+  description: "Le MacBook Pro le plus puissant jamais conçu. Avec les puces M3 Pro et M3 Max, pour une performance et des capacités encore plus décuplées. Jusqu'à 22 heures d'autonomie. Écran Liquid Retina XDR 16 pouces. Appareil photo 1080p. Son spatial. Et bien plus encore.",
+  image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp16-spacegray-select-202301?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1671304673202",
+  alt: "MacBook Pro 16 pouces"
+};
+
+export default product;
